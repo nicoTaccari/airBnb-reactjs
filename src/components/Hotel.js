1c7990a9ef7c65a7b9e3aab71c0d05be4d13d2a8
Nicolas Taccari <nicolastaccari@gmail.com>
2019-10-26 14:35:19 -0300

    added price logic to show dollar icons

--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -1,27 +1,24 @@
 import React, { Component } from "react";
+import Price from './Price'
 
 class Hotel extends Component {
   constructor(props) {
     super(props);
+
   }
 
   render() {
-      const { hotel } = this.props;
+    const { hotel } = this.props;
     return (
       <div className="card">
         <div className="card-image">
           <figure className="image is-4by3">
-            <img
-              src={hotel.photo}
-              alt={hotel.slug}
-            />
+            <img src={hotel.photo} alt={hotel.slug} />
           </figure>
         </div>
         <div className="card-content">
           <p className="title is-4">{hotel.name}</p>
-          <p>
-            {hotel.description}
-          </p>
+          <p>{hotel.description}</p>
           <div
             className="field is-grouped is-grouped-multiline"
             style={{ marginTop: "1em" }}
@@ -39,29 +36,14 @@ class Hotel extends Component {
                 <span className="tag is-medium is-info">
                   <i className="fas fa-bed"></i>
                 </span>
-                <span className="tag is-medium">{hotel.rooms} Habitaciones</span>
+                <span className="tag is-medium">
+                  {hotel.rooms} Habitaciones
+                </span>
               </div>
             </div>
             <div className="control">
               <div className="tags">
-                <span className="tag is-medium is-info">
-                  <i
-                    className="fas fa-dollar-sign"
-                    style={{ margin: "0 .125em" }}
-                  ></i>
-                  <i
-                    className="fas fa-dollar-sign"
-                    style={{ margin: "0 .125em" }}
-                  ></i>
-                  <i
-                    className="fas fa-dollar-sign"
-                    style={{ margin: "0 .125em", opacity: ".25" }}
-                  ></i>
-                  <i
-                    className="fas fa-dollar-sign"
-                    style={{ margin: "0 .125em", opacity: ".25" }}
-                  ></i>
-                </span>
+                <span className="tag is-medium is-info">{Price(hotel)}</span>
               </div>
             </div>
           </div>
@@ -79,4 +61,4 @@ class Hotel extends Component {
   }
 }
 
-export default Hotel;
+export default Hotel
