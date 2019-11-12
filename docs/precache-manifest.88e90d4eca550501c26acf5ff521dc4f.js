ef364439aa8c19b307780c9311d8025838f075b6
Nicolas Taccari <nicolastaccari@gmail.com>
2019-11-11 23:01:09 -0300

    Public site

---
+++ docs/precache-manifest.88e90d4eca550501c26acf5ff521dc4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac131b772880db3a9d44fcd54644e9b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e55efd5fc6bb72acb570",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "beba283e013f1c5710cb",
+    "url": "/static/js/2.eeb618dd.chunk.js"
+  },
+  {
+    "revision": "e55efd5fc6bb72acb570",
+    "url": "/static/js/main.d73c4e31.chunk.js"
+  },
+  {
+    "revision": "a9cf406d04f47130aebd",
+    "url": "/static/js/runtime-main.891dd56b.js"
+  }
+]);
